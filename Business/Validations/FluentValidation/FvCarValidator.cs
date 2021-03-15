c685046018367c52ff5d124b2409184c2a3aab7b
keciciahmet42 <keciciahmet42@gmail.com>
2021-03-16 02:22:10 +0300

    deneme

--- Business/Validations/FluentValidation/FvCarValidator.cs
+++ Business/Validations/FluentValidation/FvCarValidator.cs
@@ -13,7 +13,7 @@ namespace Business.Validations.FluentValidation
             RuleFor(x=>x.ModelYear).NotEmpty().WithMessage("Evet");
             RuleFor(x => x.ColorId).NotEmpty().GreaterThan(5);
             RuleFor(x => x.BrandId).NotEmpty().WithMessage("Evet");
-           
+            RuleFor(x => x.BrandId).NotEmpty().WithMessage("selam");
         }
     }
 }
